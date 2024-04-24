fbd79b3729b84cb077fb785853aa8430ce623de8
Mansik_kun <wjdalstjr730@gmail.com>
2024-04-24 13:36:49 +0900

    Add:Add url path
    detail page 로 가는url path추가

--- item/urls.py
+++ item/urls.py
@@ -5,8 +5,5 @@ app_name = "item"
 urlpatterns = [
 path('', views.list_item, name='item_list'),
 path('<int:cat_id>/',views.cat_list_item,name='cat_list'),
-
-
-
-
+path('d<int:item_id>/',views.detail_list_item,name='detail'),
 ]
