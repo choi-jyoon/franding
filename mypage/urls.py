fc0fdad4dc0a7ba194337f9444860a39518dc582
kimgibok <ruddjs33@naver.com>
2024-04-24 14:52:50 +0900

    feat:회원정보 등록,수정 기능 구현

--- mypage/urls.py
+++ mypage/urls.py
@@ -5,4 +5,6 @@ app_name='mypage'
 urlpatterns = [
     path('', views.order_index, name='order_index'),
     path('userinfo/', views.user_info, name='user_info'),
+    path('add_userinfo/', views.add_user_info, name='add_user_info'),
+    path('update_userinfo/', views.update_user_info, name='update_user_info'),
 ]
