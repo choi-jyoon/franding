2eef181dc5ce0dd97913f5290d7a1ad46d148b3c
Mansik_kun <wjdalstjr730@gmail.com>
2024-04-26 13:12:45 +0900

    Remove:Remove view
    카테고리 리스트 뷰 삭제

--- item/views.py
+++ item/views.py
@@ -8,8 +8,26 @@ from .models import *
 
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
+
+#category 정리
+#100번대->cat1
+#200번대->cat2
+#250번대->detail_cat2
+#300번대->brand
+#400번대->item_type
+
 def list_item(request):
     items = Item.objects.all()
+    
+    # 체크박스 필터링 기능 추가
+    category1_ids = request.GET.getlist('cat1')
+    category2_ids = request.GET.getlist('cat2')
+    
+    if category1_ids:
+        items = items.filter(cat1__id__in=category1_ids)
+    if category2_ids:
+        items = items.filter(cat2__id__in=category2_ids)
+    
     paginator = Paginator(items, 4)  # 한 페이지에 20개씩 표시
     page_number = request.GET.get('page')
     
@@ -24,54 +42,11 @@ def list_item(request):
         'items': page_obj,
         'cat1': Category1.objects.all(),
         'cat2': Category2.objects.all(),
+        'selected_cat1':[int(cat_id) for cat_id in category1_ids],
+        'selected_cat2':[int(cat_id) for cat_id in category2_ids],
     }
     return render(request, 'item/list.html', context)
 
-
-
-
-#category 정리
-#100번대->cat1
-#200번대->cat2
-#250번대->detail_cat2
-#300번대->brand
-#400번대->item_type
-def cat_list_item(request,cat_id):
-    if int(cat_id)>100 and int(cat_id)<200:
-        cat_id = cat_id-100
-        cat = Category1.objects.get(id=cat_id)
-        #아이템 모델에서 카테고리 1 값이 cat과 동일한 데이터를 찾아서 반환
-        context={
-        "key":Item.objects.filter(cat1 = cat)
-        } #리퀘스트, 템플릿주소, 콘텍스트 를 반환
-        return render(request,'item/cat_list.html',context)
-    elif int(cat_id)>200 and int(cat_id)<300:
-        cat_id = cat_id-200
-        cat= Category2.objects.get(id=cat_id)
-        context={
-        "key":Item.objects.filter(cat2 = cat)
-        }
-        return render(request,'item/cat_list.html',context)
-    elif int(cat_id)==300:
-        cat = Item.objects.all()
-        context={
-        "key":cat
-        }
-        return render(request,'item/cat_list.html',context)
-    elif int(cat_id)>300 and int(cat_id)<400:
-        cat_id = cat_id-300
-        cat = Brand.objects.get(id=cat_id)
-        context={
-        "key":Item.objects.filter(brand = cat)
-        }
-        return render(request,'item/cat_list.html',context)
-    elif int(cat_id)>400 and int(cat_id)<500:
-        cat_id = cat_id-400
-        cat = ItemType.objects.get(id=cat_id)
-        context={
-        "key":Item.objects.filter(item_type = cat)
-        }
-        return render(request,'item/cat_list.html',context)
 def detail_list_item(request,item_id):
     model=Item.objects.get(id=item_id)
     context={
