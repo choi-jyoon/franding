c97cc02b8a37c86d1c497107e38c54b89f56fb66
Mansik_kun <wjdalstjr730@gmail.com>
2024-04-30 10:34:12 +0900

    Feat:Add urls
    URL path 추가

--- item/urls.py
+++ item/urls.py
@@ -4,5 +4,7 @@ from . import views
 app_name = "item"
 urlpatterns = [
 path('', views.list_item, name='item_list'),
+path ('brand/',views.list_item, name='brand_list'),
+path ('perfume/',views.list_item, name='perfume_list'),
 path('<int:item_id>/',views.detail_list_item,name='detail'),
 ]
