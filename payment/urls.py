c806523871122b58b449d53a9019e0ab24614dbe
SinMoonGh <109773691+SinMoonGh@users.noreply.github.com>
2024-04-30 11:52:33 +0900

    done

--- payment/urls.py
+++ payment/urls.py
@@ -5,8 +5,7 @@ from .views import payment_list, PaymentDoneView
 app_name = 'payment'
 
 urlpatterns = [
-    # Add your URL patterns here
+    # Add your URL patterns heree
     path('', payment_list, name='payment_list'),
-    path('payments/', include('payments.urls')),
     path('payment_done/', PaymentDoneView.as_view(), name='payment_done')
 ]
