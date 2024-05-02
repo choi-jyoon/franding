d95f300f3fac8ee640cd1ba3da16685443fe340c
choi-jyoon <72444141+choi-jyoon@users.noreply.github.com>
2024-05-02 14:11:42 +0900

    Add: UserAddInfo -user_name 추가

--- mypage/models.py
+++ mypage/models.py
@@ -5,6 +5,7 @@ from django.contrib.auth.models import User
 
 class UserAddInfo(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
+    user_name = models.CharField(max_length=50, default='ooo')
     address = models.TextField()
     phone = models.CharField(max_length=20)
     profile_img = models.URLField(null=True, default=' ')
