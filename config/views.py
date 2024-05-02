aba4085f7ab0ad1dfb5394e24e0b94e1770530cc
choi-jyoon <72444141+choi-jyoon@users.noreply.github.com>
2024-05-02 11:22:26 +0900

    Feat: 검색창 페이지네이션

--- config/views.py
+++ config/views.py
@@ -4,10 +4,12 @@ from django.contrib.auth.forms import UserCreationForm
 from django.urls import reverse_lazy
 from item.models import Item
 from django.db.models import Q
+from django.core.paginator import Paginator
 # Create your views here.
 
 def index(request):
     objects = Item.objects.all().order_by('-id')
+    
     context = {
         'item_list' : objects
     }
@@ -43,10 +45,15 @@ def searchItem(request):
     elif select_option == 'desc':
         objects = Item.objects.filter(Q(summary__icontains=search_word)|Q(description__icontains=search_word))
         
+    paginator = Paginator(objects, 16) # 한 페이지 당 20개의 아이템이 표시되도록 설정합니다.
+
+    page_number = request.GET.get('page', 1) # URL에서 page 넘버를 가져옵니다.
+    page_obj = paginator.get_page(page_number) # 해당 페이지의 아이템들만 page_obj에 저장합니다.
+
     context = {
         'select_option':select_option,
         'search_word':search_word,
-        'result': objects
+        'page_obj': page_obj
     }
     
     return render(request, 'search.html', context)
