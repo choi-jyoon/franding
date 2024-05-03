56807ed0161804aba0754057e485790f8a7db949
SinMoonGh <109773691+SinMoonGh@users.noreply.github.com>
2024-05-03 11:11:27 +0900

    장바구니 상품 최소 수량 1보다 밑으로 가지 않게 동작만 되도록 적용했습니다.#

--- cart/views.py
+++ cart/views.py
@@ -20,14 +20,8 @@ from django.http import JsonResponse
 #     return cart_id
 
 
-# cart_id 신경쓰지 말것
-# usr_id 로 상품을 조회하자
-# status로 구매여부를 확인하자
-def add_cart(request,) -> Any:
-    # 카트아이템에 담자
-    # 카트아이템이 있는지 확인을 해야한다.
-    # 카트 아이템이 아직 없다.
-    # 카트아이템에 담을려면 뭐가 필요할까?
+# 장바구니에 상품 추가
+def add_cart(request,) -> Any:    
     if request.method == 'POST':
 
         # 유저정보: request.user, 담을 상품 : item, 수량 : click 했을 시 생김, 상태 : click을 했는데 결제를 안했을 시 False
@@ -36,42 +30,17 @@ def add_cart(request,) -> Any:
         item_id = request.POST['item_id'] # 유저가 담은 상품을 가져왔다.
         item = get_object_or_404(Item, id=item_id) # Item모델에 해당 상품이 없다면 에러 발생.
         # 카트 아이템에 담았지만 아직 결제를 하지 않은 상태이다.
-        status = False
-        
-
-        # 카트 아이템을 만들어야 한다. 여기까지 만들어보자.
-        
+        status = False     
+            
         
         cart = Cart.objects.create(user=user, item=item, status=status, amount=0)
         cart.amount += 1
         cart.save()
     return HttpResponseRedirect(request.META.get('HTTP_REFERER', '/')) # 현재 페이지 그래로 유지
 
-
-# 카트 삭제 기능 payment로 옮겼습니다!!
-# def delete_cart(request):
-#     if request.method == 'GET':
-#         cart_id = request.GET['cart_id']
-#         cart = Cart.objects.get(pk=cart_id)
-#         cart.delete()
-#         return redirect('cart:cart_detail')
-
-
-    # 만약에 추가로 담을거면 있는 카트에 담아야 한다.  
-    # try:
-    #     cart = Cart.objects.get(user=user, status=False)
-    #     cart.amount += 1
-    #     cart.save()
-    # except Cart.DoesNotExist:
-    #     cart = Cart(user=user, item=item, status=status, amount=amount)
-    #     cart.save()
-    # return redirect(request, 'cart/cart.html', {'cart': cart})
-
       
-# 템플릿에 표시해야 함.
-# 장바구니 페이지를 만들 때 새로운 함수를 만들어서 filter를 user, status 값을 가져와서 쭉 
-# 뽑아주는 형태로 만들면 될 것 같다.
 
+# 장바구니 페이지
 def cart_detail(request, total_price=0):
     cart = Cart.objects.filter(user=request.user, status=False).order_by('user', '-status', '-id')
 
@@ -97,12 +66,10 @@ def accept_ajax(request, total_price=0):
         # 수량이 1개 이상일 때만 저장
         if cart.amount >= 1:
             cart.save()
+        elif cart.amount == 0:
+            cart.amount += 1
         
-        # total_price 만들기
-        # total_price는 어떤 모양이어야 되는가? int형 값을 할당시켜야 한다.
-        # cart_total_count를 하나씩 c에 할당을 시키면 c는 cart[0]이 출력이 된다.
-        # cart[0]의 가격 * 수량을 해서 total_price에 더해준다.
-        # total_price를 context에 담아서 반환해준다.
+        
         cart_total_count = Cart.objects.filter(user=request.user, status=False).order_by('user', '-status', '-id')
 
         for c in cart_total_count:
@@ -118,11 +85,4 @@ def accept_ajax(request, total_price=0):
         }
         return JsonResponse(context)
 
-# 컨텍스트에 다가 total_price를 담아서 보내자
-# 상품 price 가져온다.
-# for 문을 돌려서 price 아이템 가격을 1개씩 꺼내와서 total_price에 넣어준다.
-# total_price를 컨텍스트에 담아서 json으로 반환한다.
-# funtion에서 total_price키 값을 받아서 출력해준다??
 
-
-# 결제창
