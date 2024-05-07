e967ae74e4f6f807e36ce5ee764b9c34c49ec1a2
kimgibok <ruddjs33@naver.com>
2024-05-07 17:56:49 +0900

    feat: franding설문조사기능 구현

---
+++ event/forms.py
@@ -0,0 +1,10 @@
+from django import forms
+
+class PerfumeForm(forms.Form):
+    intensity = forms.ChoiceField(choices=[('subtle', 'Subtle'), ('strong', 'Strong')])
+    season = forms.ChoiceField(choices=[('spring', 'Spring'), ('summer', 'Summer'), ('fall', 'Fall'), ('winter', 'Winter')])
+    gender = forms.ChoiceField(choices=[('male', 'Male'), ('female', 'Female'), ('unisex', 'Unisex')])
+    age_range = forms.ChoiceField(choices=[('~19', '19세 이하'), ('(20~24)', '20~24'), ('(25~29)', '25~29'), ('(30~34)', '30~34'), ('35~', '35세 이상')])
+    price_range = forms.ChoiceField(choices=[('1', '7만원 이하'), ('2', '7~15만원'), ('3', '15~22만원'), ('4', '22만원 이상'), ('5', '상관없음')])
+    cat1 = forms.ChoiceField(choices=[('시트러스', '시트러스'), ('플로럴', '플로럴'), ('스위트', '스위트'), ('우디', '우디'), ('머스크', '머스크')])
+    cat2 = forms.ChoiceField(choices=[('Lovely', 'Lovely'), ('Casual', 'Casual'), ('Cool', 'Cool'), ('Femine', 'Femine')])
--- event/urls.py
+++ event/urls.py
@@ -4,5 +4,5 @@ from . import views
 app_name='event'
 urlpatterns = [
     path('', views.index, name='index'),
-    
+    path('recommend-perfume/', views.recommend_perfume, name='recommend-perfume'),
 ]
