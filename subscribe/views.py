e23a2cf54cd254ecbe4981c2cedf3287604145ee
choi-jyoon <72444141+choi-jyoon@users.noreply.github.com>
2024-06-03 19:28:12 +0900

    Feat: 구독 상품 선별 로직 추가

--- subscribe/views.py
+++ subscribe/views.py
@@ -3,6 +3,9 @@ from .models import Keyword, Subscribe, SubscribeKeyword
 from django.utils import timezone
 from django.contrib.auth.decorators import login_required
 from collections import defaultdict
+from item.models import Item
+from collections import Counter
+import random
 # Create your views here.
 
 @login_required
@@ -44,10 +47,32 @@ def index(request):
 def detail(request, pk):
     
     subscribe = Subscribe.objects.get(pk=pk)
-    sub_keyword = SubscribeKeyword.objects.filter(subscribe=subscribe)
+    sub_keywords = SubscribeKeyword.objects.filter(subscribe=subscribe)
+    
+    # 키워드별로 카테고리 1, 2 리스트 저장
+    cat1 = []
+    cat2 = []
+    for sub_keyword in sub_keywords:
+        cat1.append(sub_keyword.keyword.category1)
+        cat2.append(sub_keyword.keyword.category2)
+    
+    # 가장 많이 나온 값 선택
+    most_cat1 = Counter(cat1).most_common(1)[0][0]
+    most_cat2 = Counter(cat2).most_common(1)[0][0]
+    
+    # 가장 많이 나온 카테고리에 맞는 아이템 선정
+    items = Item.objects.filter(cat1 = most_cat1, cat2 = most_cat2, item_type = '1')
+    if items.exists():
+        item = random.choice(items)
+    # 카테고리에 맞는 아이템 없을 시 카테고리1만 고려해서 랜덤 선택
+    else:
+        item = random.choice(Item.objects.filter(cat1 = most_cat1, item_type = '1'))
+        
+    subscribe.item = item
+    subscribe.save()
     
     context = {
-        'subcribe_object':subscribe,
-        'subscriptions' : sub_keyword,
+        'subscribe_object':subscribe,
+        'subscriptions' : sub_keywords,
     }
     return render(request, 'subscribe/detail.html', context)
