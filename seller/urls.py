0ef1ae89dd353cc1f894aa208f6724289fd4c1df
kimgibok <ruddjs33@naver.com>
2024-06-05 17:55:45 +0900

    feat: 상품세부페이지(상품정보, 리뷰보기)

--- seller/urls.py
+++ seller/urls.py
@@ -1,15 +1,13 @@
 from django.urls import path
-from .views import item_create, item_list, item_detail, item_update, item_delete, seller_page
 from . import views
-from .views import ItemListView
 
 app_name='seller'
 
 urlpatterns = [
-    path('', views.seller_page, name='seller_index'),  # 판매자 페이지
+    path('', views.seller_index, name='seller_index'),  # 판매자 페이지
     path('item-create/', views.item_create, name='item_create'),  # 상품 추가 페이지
-    path('item-list/', ItemListView.as_view(), name='item_list'),  # 상품 조회 페이지
-    path('item-update/<int:pk>/', views.item_update, name='item_update'), # 상품 업데이트 페이지
+    # path('item-list/', ItemListView.as_view(), name='item_list'),  # 상품 조회 페이지
+    path('item-detail/<int:pk>/', views.item_detail, name='item_detail'), # 상품 업데이트 페이지
     path('item-delete/<int:pk>/', views.item_delete, name='item_delete'),  # 상품 삭제 페이지
     path('add-brand/', views.add_brand, name='add_brand'),
 ]
