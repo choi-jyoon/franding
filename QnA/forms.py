71352d4848cf397a2ec04d9a58407a754a36b1ae
SinMoonGh <109773691+SinMoonGh@users.noreply.github.com>
2024-06-05 17:43:20 +0900

    q&a 게시판 마이페이지랑 연결#

--- QnA/forms.py
+++ QnA/forms.py
@@ -4,7 +4,7 @@ from .models import Question, Answer
 class QuestionForm(forms.ModelForm):
     class Meta:
         model = Question
-        fields = ['id', 'title', 'content']
+        fields = ['id', 'item_id', 'title', 'content']
 
 class AnswerForm(forms.ModelForm):
     class Meta:
