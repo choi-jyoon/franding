f0de66ccd2ab70765b75d373b9358a84fa57c234
NAMYUNWOO <ajtmxod000@naver.com>
2024-06-14 09:50:48 +0900

    상품 조회 수 기능 보류

--- config/settings.py
+++ config/settings.py
@@ -179,7 +179,7 @@ ALLOWED_HOSTS = ['testserver', 'localhost', '127.0.0.1', '[::1]']
 CACHES = {
     'default': {
         'BACKEND': 'django_redis.cache.RedisCache',
-        'LOCATION': 'redis://127.0.0.1:6379/1',
+        'LOCATION': 'redis://127.0.0.1:6379/1',  # Redis 서버 위치
         'OPTIONS': {
             'CLIENT_CLASS': 'django_redis.client.DefaultClient',
         }
