583d1b6dcc772a806a2b7b1ad81671a9bb8658b3
choi-jyoon <72444141+choi-jyoon@users.noreply.github.com>
2024-06-17 14:41:32 +0900

    Feat: 아이디, 비밀번호 찾기

--- config/settings.py
+++ config/settings.py
@@ -225,10 +225,10 @@ SOCIALACCOUNT_PROVIDERS = {
     }
 }
 
-# EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-# EMAIL_HOST = 'smtp.gmail.com'
-# EMAIL_PORT = 587
-# EMAIL_USE_TLS = True
-# EMAIL_HOST_USER = 'franding3@gmail.com'
-# EMAIL_HOST_PASSWORD = EMAIL_PASSWORD
-# DEFAULT_FROM_EMAIL = 'franding3@gmail.com'
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'franding3@gmail.com'
+EMAIL_HOST_PASSWORD = EMAIL_PASSWORD
+DEFAULT_FROM_EMAIL = 'franding3@gmail.com'
