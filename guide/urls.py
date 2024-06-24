34fce78eae91abb6bb075011f27d57a68f2bf891
NAMYUNWOO <ajtmxod000@naver.com>
2024-06-24 21:58:29 +0900

    임시저장

--- guide/urls.py
+++ guide/urls.py
@@ -8,7 +8,7 @@ app_name = 'guide'  # 네임스페이스 설정
 
 urlpatterns = [
     path('', views.guide_list, name='guide_list'),
-    path('brandstory/', views.brand_story, name='brand Story'),
+    path('brandstory/', views.brand_story, name='brand_attitude'),
     path('description/', views.description, name='description'),
     path('pleasant/', views.pleasant, name='pleasant'),
     path('scent/', views.scent, name='scent'),
